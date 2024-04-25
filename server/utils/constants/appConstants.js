d9e8432ea19250ed1e590f7d3062ce0a8d2525ad
Adarsh Gupta <csedsadarsh@gmail.com>
2024-04-26 01:07:17 +0530

    Add appConstants and default_code.json files, and validateCode.js validator

---
+++ server/utils/constants/appConstants.js
@@ -0,0 +1,12 @@
+module.exports = {
+    // ROOM Routes
+    ROOMS: {
+        CREATE: '/rooms/create',
+        FETCH: '/rooms/fetch',
+        UPDATE: '/rooms/update',
+        DELETE: '/rooms/delete',
+    },
+    CODE: {
+        EXECUTE: '/code/execute',
+    }
+};
