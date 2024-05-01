50ca8f629c29e8a7a76f5228c9925e7e9bf12341
Adarsh Gupta <csedsadarsh@gmail.com>
2024-05-01 21:12:27 +0530

    Add Navbar component and update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/Home';
 import './App.css';
+import Navbar from './components/common/Navbar';
 
 function App() {
   return (
     <Router>
-      <div>
+      <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
+        <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
+
         </Routes>
       </div>
     </Router>
